5c920af5be67a7e8c39d00bc2ba8f1d78906c93c
adautobarros <adauto.brrs@gmail.com>
2014-06-02 09:19:04 -0300

    s

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
- document.addEventListener("deviceready", onDeviceReady, false);
-  function onDeviceReady() {
-   $("#btOk").click(function(){
-        var nome = $("#txtNome").val();
-		 if(nome.length == 0)
-			alert("preencha seu nome no campo apresentado na tela.");
-		 else
-			 alert("Boas Vindas  "+nome+"!!");
-	 });
-	}
+document.addEventListener("deviceready", onDeviceReady, false);
 
+function onDeviceReady() {
+    $("#btOk").click(function(){
+        var nome = $("#txtNome").val();
+        if(nome.length == 0)
+        {
+            alert("Preencha seu nome no campo apresentado na tela.");
+        }
+        else
+        {
+            alert("Boas Vindas  "+nome+"!!");
+        }
+    });
+}
